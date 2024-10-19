9f383724479b2f1778763e42dfa96fff5c0e314c
Ziad Alaa <ziadalaa955@gmail.com>
2024-10-19 14:34:35 +0300

    create tuneitem widget

--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:tunes_app/widgets/tune_item.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
   static String id = 'homeView';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color(0xff223238),
+        title: const Text(
+          'Flutter Tunes',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: const Column(
+        children: [
+          TuneItem(
+            color: Color(0xffEE2B3A),
+          ),
+          TuneItem(
+            color: Color(0xffF3932E),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/tune_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TuneItem extends StatelessWidget {
+  const TuneItem({super.key, required this.color});
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: color,
+        height: double.infinity,
+      ),
+    );
+  }
+}
